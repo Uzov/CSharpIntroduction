feec5e60ffb0b5cfaf5c0265cf7bb36ab72e60ac
Evgeny Uzov <uzov@cg.ru>
2023-06-10 20:23:38 +0300

    Добавьте файлы проекта.

---
+++ Lesson1.1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7->max = 7
+a = 2 b = 10->max = 10
+a = -9 b = -3->max = -3*/
+
+int a;
+int b;
+
+a = 5; b=7;
+if (a > b) { Console.WriteLine(a); } else { Console.WriteLine(b); }
+
+a = 2; b = 10;
+int c = a > b ? a : b;
+Console.WriteLine(c);
+
+a = -9; b = -3;
+if (a > b) { Console.WriteLine(a); } else { Console.WriteLine(b); }
+
