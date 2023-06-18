43f7dec00462dd974b7b51de1d669fe9036443bc
Evgeny Uzov <uzov@cg.ru>
2023-06-18 12:54:43 +0300

    Выполнено задание к Уроку 3

---
+++ Lesson3.19/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+int number1 = 14212;
+Palindrom(number1);
+
+int number2 = 12821;
+Palindrom(number2);
+
+int number3 = 23432;
+Palindrom(number3);
+
+void Palindrom(int num)
+{
+    if ((num % 10 == num / 10000) && ((num % 100 / 10 == num / 1000 % 10))) Console.WriteLine($"Число {num} - палиндром.");
+        else Console.WriteLine($"Число {num} - не палиндром.");
+}
---
+++ Lesson3.21/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+ */
+float result = (float)Math.Sqrt(Math.Pow(3 - 2, 2) + Math.Pow(6 - 1, 2) + Math.Pow(8 - (-7), 2));
+Console.WriteLine(Math.Round(result, 2));
+float result1 = (float)Math.Sqrt(Math.Pow(7 - 1, 2) + Math.Pow((-5) - (-1), 2) + Math.Pow(0 - 9, 2));
+Console.WriteLine(Math.Round(result1, 2));
