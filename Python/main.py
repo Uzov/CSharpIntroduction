998708c4a9142de1664539178795c32172d7065b
Evgeny Uzov <uzov@cg.ru>
2023-07-06 23:42:47 +0300

    Сделана задача 54

---
+++ Python/main.py
@@ -0,0 +1,9 @@
+def sort_row():
+    inputList = [[1, 4, 7, 2], [5, 9, 2, 3], [8, 4, 2, 4]]
+    for i in range(3):
+        inputList[i].sort()
+    print(inputList)
+
+
+if __name__ == '__main__':
+    sort_row()
