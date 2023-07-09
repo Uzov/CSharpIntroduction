bf78b305537128429262333920bcce55b17b3e6d
Evgeny Uzov <uzov@cg.ru>
2023-07-10 00:05:50 +0300

    Выполнено задание к Уроку 9

--- Lesson9/Program.cs
+++ Lesson9/Program.cs
@@ -11,7 +11,8 @@ M = 4; N = 8. -> 30
 m = 3, n = 2 -> A(m,n) = 9
 m = 2, n = 3 -> A(m,n) = 29
  */
-
+using System.Threading;
+delegate void Akkerman();
 internal class Program
 {
     private static void Main(string[] args)
@@ -39,21 +40,41 @@ internal class Program
             return n + Sum(m, n - 1);
         }
 
-        Console.WriteLine(Sum(4,8));
+        Console.WriteLine(Sum(4, 8));
         Console.WriteLine();
 
         Console.WriteLine(Sum(1, 15));
         Console.WriteLine();
-        Console.WriteLine();
 
         //Задача 68
-        int Akkerman(int n, int m)
+        /*
+        Проблема Stack overflow решается созданием нового потока с указанием размера стека:
+        Thread(ThreadStart, Int32): в дополнение к делегату ThreadStart принимает числовое значение, 
+        которое устанавливает размер стека, выделяемого под данный поток. Делегаты - объекты, которые указывают на методы, т.е.
+        что-то типа указателей (*pointer) в Си. Вместо делегата ThreadStart вставляется лямбда-выражение, т.е. 
+        безымянная функция вида: (список_параметров) => выражение. С точки зрения типа данных лямбда-выражение 
+        представляет делегат. Это самое лямбда - выражение внутри содержит рекурсивно вычисляемую функцию Аккермана вместе с выводом 
+        результатов на консоль. 
+        N.B. я имею понятие что делаю (использовал google и metanit).
+         */
+        new Thread(() =>
         {
-            return 0;
-        }
+            uint Akk(uint n, uint m)
+            {
+                if (n == 0)
+                    return m + 1;
+                else
+                  if ((n != 0) && (m == 0))
+                    return Akk(n - 1, 1);
+                else
+                    return Akk(n - 1, Akk(n, m - 1));
+            }
 
-        Console.WriteLine(Akkerman(3, 2));
-        Console.WriteLine();
-        Console.WriteLine(Akkerman(2, 3));
+            Console.WriteLine(Akk(3, 2));
+            Console.WriteLine();
+            Console.WriteLine(Akk(2, 3));
+            Console.WriteLine();
+            Console.WriteLine(Akk(4, 3));//Переполнение стека, даже если его увеличить до 10Мб!
+        }, 2097152 * 5).Start();//Размер стека 10Мб
     }
 }
--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,1 +1,7 @@
-﻿while (true) break;
+﻿Message mes;            // 2. Создаем переменную делегата
+mes = Hello;            // 3. Присваиваем этой переменной адрес метода
+mes();                  // 4. Вызываем метод
+
+void Hello() => Console.WriteLine("Hello METANIT.COM");
+
+delegate void Message();
